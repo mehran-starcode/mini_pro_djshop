21fef0ca8c37bb123da06faad357ddf84b4510a9
mehran-starcode <mehran2bb@gmail.com>
2024-05-15 23:11:16 +0330

    Delete Old Project and Create a New Project

--- scripts/run.sh
+++
@@ -1,3 +0,0 @@
-
-
-RUN apt install graphviz
--- src/djshop/apps/catalog/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-
-# Create your views here.
-
-
-def catalog_page(request):
-    return render(request, 'base.html')
-
