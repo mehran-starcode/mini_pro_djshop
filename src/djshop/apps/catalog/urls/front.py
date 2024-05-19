c41d24a9bf5231f82b81743ab83daab87026a5fe
mehran-starcode <mehran2bb@gmail.com>
2024-05-19 14:14:31 +0330

    Create CategorySerializer for Category Model

--- src/djshop/apps/catalog/urls/front.py
+++ src/djshop/apps/catalog/urls/front.py
@@ -1,14 +1,15 @@
 from django.urls import path
-from djshop.apps.catalog.views.front import Category_CB, category
+# from djshop.apps.catalog.views.front import Category_CB, category
 
 # from rest_framework.routers import SimpleRouter
-#
-# from src.djshop.apps.catalog.views.front import CategoryViewSet
-#
+
+# from djshop.apps.catalog.views.front import CategoryViewSet
+
 # router = SimpleRouter()
 # router.register(prefix='categories', viewset=CategoryViewSet)
 
 urlpatterns = [
-    path('simple-classbase-view', Category_CB.as_view()),
-    path('funcbase-view', category, {}),
+    # path('simple-classbase-view', Category_CB.as_view()),
+    # path('funcbase-view', category, {}),
+    # path('classbase-viewset', CategoryViewSet.as_view({'get': 'list'}))
 ]
--- src/djshop/apps/catalog/views/front.py
+++ src/djshop/apps/catalog/views/front.py
@@ -2,20 +2,22 @@ from django.http import HttpResponse
 from django.views import View
 from django.shortcuts import render
 from djshop.apps.catalog.models import Category
+
+
 # from rest_framework import viewsets
 # from djshop.apps.catalog.serializers.front import CategorySerializer
-
-
+#
+#
 # class CategoryViewSet(viewsets.ReadOnlyModelViewSet):
 #     queryset = Category.objects.all()
 #     serializer_class = CategorySerializer
 
 
-class Category_CB(View):
-
-    def get(self, request):
-        return HttpResponse("Simple Class Base View Result")
-
-
-def category(request):
-    return HttpResponse('This Result is from Func Base View')
+# class Category_CB(View):
+#
+#     def get(self, request):
+#         return HttpResponse("Simple Class Base View Result")
+#
+#
+# def category(request):
+#     return HttpResponse('This Result is from Func Base View')
