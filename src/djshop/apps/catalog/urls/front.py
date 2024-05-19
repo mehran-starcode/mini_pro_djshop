ec5808633affbf4c943db93636c5d705169f5fba
mehran-starcode <mehran2bb@gmail.com>
2024-05-19 14:32:40 +0330

    Create new Router and register it for CategoryViewSet and Add it to urlpatterns of urls/front.py file

--- src/djshop/apps/catalog/urls/front.py
+++ src/djshop/apps/catalog/urls/front.py
@@ -1,15 +1,15 @@
 from django.urls import path
 # from djshop.apps.catalog.views.front import Category_CB, category
 
-# from rest_framework.routers import SimpleRouter
+from rest_framework.routers import SimpleRouter
 
 from djshop.apps.catalog.views.front import CategoryViewSet
 
-# router = SimpleRouter()
-# router.register(prefix='categories', viewset=CategoryViewSet)
+router = SimpleRouter()
+router.register(prefix='categories', viewset=CategoryViewSet)
 
 urlpatterns = [
     # path('simple-classbase-view', Category_CB.as_view()),
     # path('funcbase-view', category, {}),
     path('classbase-viewset', CategoryViewSet.as_view({'get': 'list'}))
-]
+] + router.urls
