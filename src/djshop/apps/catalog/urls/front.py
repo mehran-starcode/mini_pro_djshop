c2e345366850cb425873291b85d498d9be341528
mehran-starcode <mehran2bb@gmail.com>
2024-05-19 14:26:46 +0330

    Set path & Config URL Constructor for Run CategoryViewSet

--- src/djshop/apps/catalog/urls/front.py
+++ src/djshop/apps/catalog/urls/front.py
@@ -3,7 +3,7 @@ from django.urls import path
 
 # from rest_framework.routers import SimpleRouter
 
-# from djshop.apps.catalog.views.front import CategoryViewSet
+from djshop.apps.catalog.views.front import CategoryViewSet
 
 # router = SimpleRouter()
 # router.register(prefix='categories', viewset=CategoryViewSet)
@@ -11,5 +11,5 @@ from django.urls import path
 urlpatterns = [
     # path('simple-classbase-view', Category_CB.as_view()),
     # path('funcbase-view', category, {}),
-    # path('classbase-viewset', CategoryViewSet.as_view({'get': 'list'}))
+    path('classbase-viewset', CategoryViewSet.as_view({'get': 'list'}))
 ]
