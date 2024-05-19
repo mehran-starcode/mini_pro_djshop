6cda7fc459eb08af10a5324b104f7434e7128e7e
mehran-starcode <mehran2bb@gmail.com>
2024-05-19 14:24:15 +0330

    Create CategoryViewSet as a Class Base View for Category Model

--- src/djshop/apps/catalog/views/front.py
+++ src/djshop/apps/catalog/views/front.py
@@ -3,21 +3,19 @@ from django.views import View
 from django.shortcuts import render
 from djshop.apps.catalog.models import Category
 
-
-# from rest_framework import viewsets
-# from djshop.apps.catalog.serializers.front import CategorySerializer
-#
-#
-# class CategoryViewSet(viewsets.ReadOnlyModelViewSet):
-#     queryset = Category.objects.all()
-#     serializer_class = CategorySerializer
+from rest_framework import viewsets
+from djshop.apps.catalog.serializers.front import CategorySerializer
 
 
-# class Category_CB(View):
+class CategoryViewSet(viewsets.ReadOnlyModelViewSet):  # &              ( ViewSet )
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+# class Category_CB(View):                        (  Simple Class Base View  )
 #
 #     def get(self, request):
 #         return HttpResponse("Simple Class Base View Result")
 #
 #
-# def category(request):
+# def category(request):                          (  Func Base View  )
 #     return HttpResponse('This Result is from Func Base View')
